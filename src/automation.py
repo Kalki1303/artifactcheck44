d9088a9ca3e205d0c0e922b6fae9462928fc1723
Kalki1303 <deepa13kalki@gmail.com>
2024-02-16 19:18:50 +0530

    choer: added dummy postman url

--- src/automation.py
+++ src/automation.py
@@ -4,10 +4,12 @@ from locust import HttpUser, task, between
 class QuickstartUser(HttpUser):
     wait_time = between(1, 5)
     #host= "http://localhost:8089/"
+    
 
     @task
     def hello_world(self):
-        response = self.client.get("/todos/1")
+        self.url ="https://postman-echo.com/get?test=123"
+        response = self.client.get(self.url)
         assert response.status_code == 200,f"request failed"
         #("https://docs.locust.io/en/stable/quickstart.html")
        
