a22f8b73f8a9d7e997746fd73c73074806d008ee
Kalki1303 <deepa13kalki@gmail.com>
2024-02-16 18:54:46 +0530

    chore: added python file

---
+++ src/automation.py
@@ -0,0 +1,11 @@
+import time
+from locust import HttpUser, task, between
+
+class QuickstartUser(HttpUser):
+    wait_time = between(1, 5)
+    host= "http://localhost:8089/"
+
+    @task
+    def hello_world(self):
+        self.client.get("https://docs.locust.io/en/stable/quickstart.html")
+        #self.client.get("/world")
