ffd1011b4a8fabd596695fcce5e9d52818c4b67c
Kalki1303 <deepa13kalki@gmail.com>
2024-02-16 19:04:03 +0530

    chor:api file updated

--- src/automation.py
+++ src/automation.py
@@ -3,9 +3,11 @@ from locust import HttpUser, task, between
 
 class QuickstartUser(HttpUser):
     wait_time = between(1, 5)
-    host= "http://localhost:8089/"
+    #host= "http://localhost:8089/"
 
     @task
     def hello_world(self):
-        self.client.get("https://docs.locust.io/en/stable/quickstart.html")
+        response = self.client.get("/api/endpoint")
+        assert response.status_code == 200,f"request failed"
+        #("https://docs.locust.io/en/stable/quickstart.html")
        
